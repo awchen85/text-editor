7fb01133d0a399ffb88453194f23f5e5cbfd7ae2
awchen <awchen85@gmail.com>
2022-12-10 10:43:59 -0500

    fixed service worker, indexDB. launches as PWA!

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-const jateDb = await openDB('jate_db', 1);
+const jateDb = await openDB('jate', 1);
 
 const tx = jateDb.transaction('jate', 'readwrite');
 
